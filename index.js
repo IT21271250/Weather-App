ce5765814db5ba603047cd93fcc38c19c8060e2d
champika ranaweera <cranaweera96@gmail.com>
2024-10-27 23:40:02 +0530

    .

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {})
