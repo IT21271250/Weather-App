c0fa7d148adc535c84ff770cfafe04d1af9f3626
champika ranaweera <cranaweera96@gmail.com>
2024-10-30 23:03:27 +0530

    .

--- index.js
+++ index.js
@@ -8,5 +8,41 @@ search.addEventListener('click', () => {
 
     const APIKey = 'Api Key';
     const city = document.querySelector('.search-box input').value;
+    
+    if (city === '')
+        return;
 
-})
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
+
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+        });
+
+
+});
