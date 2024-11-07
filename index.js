8a67ed4d6cc947e4e0030cc0cf671550f1342d64
champika ranaweera <cranaweera96@gmail.com>
2024-11-07 23:52:41 +0530

    //

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ search.addEventListener('click', () => {
             // Select DOM elements
             
             const image = document.querySelector('.weather-box img');
-            const temperature = document.querySelector('.weather-box .temperature');
+            // const temperature = document.querySelector('.weather-box .temperature');
             const description = document.querySelector('.weather-box .description');
             const humidity = document.querySelector('.weather-details .humidity span');
             const wind = document.querySelector('.weather-details .wind span');
